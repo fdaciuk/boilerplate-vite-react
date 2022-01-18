e0b2061108cb502d09477507dcf377305a4b34b4
Fernando Daciuk <f.daciuk@gmail.com>
2022-01-18 07:20:35 -0300

    new installations:
    - eslint (standard, react-app, hooks, TS)
    - husky
    - npm scripts for lint, lint:fix and type-check
    - lint fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsConfigPaths from 'vite-tsconfig-paths'
+import { EsLinter, linterPlugin } from 'vite-plugin-linter'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), tsConfigPaths()],
-})
+export default defineConfig(configEnv => ({
+  plugins: [
+    react(),
+    tsConfigPaths(),
+    linterPlugin({
+      include: ['./src/**/*.{ts,tsx}'],
+      linters: [new EsLinter({ configEnv })],
+    }),
+  ],
+}))
