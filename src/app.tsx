bf9749e8089c66314b5bdd323f62d49a134887ab
Fernando Daciuk <f.daciuk@gmail.com>
2022-01-19 08:17:04 -0300

    test configuration with jest

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import logo from './logo.svg'
-import './App.css'
+import './app.css'
 
 function App () {
   const [count, setCount] = useState(0)
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import App from '@/App'
+import App from '@/app'
 
 ReactDOM.render(
   <StrictMode>
