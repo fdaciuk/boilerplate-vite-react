4c28da2b4599f4dc7f4cf00c46bdcd6f83072680
Gabriel Pinheiro <gabrielpsilvas@live.com>
2022-03-31 20:08:47 -0300

    Updated to React version 18 and deps

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,17 @@
 import { StrictMode } from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
+import { createRoot } from 'react-dom/client'
 import App from '@/app'
+import './index.css'
 
-ReactDOM.render(
+const rootElement = document.querySelector('[data-js="root"]')
+
+if (!rootElement) {
+  throw new Error('Failed to find the root element')
+}
+
+const root = createRoot(rootElement)
+root.render(
   <StrictMode>
     <App />
   </StrictMode>,
-  document.querySelector('[data-js="root"]'),
 )
