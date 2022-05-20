207be7b1d99d4d17d92030c78d4c4842dac59629
SallesCosta <newcapital.in@gmail.com>
2022-05-20 13:17:40 +0200

    fix: named import app.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from '@/app'
+import { App } from '@/app'
 import './index.css'
 
 const rootElement = document.querySelector('[data-js="root"]')
